50d686ce600183078915d690f58225b6afb584ac
Alexey Bezrukov <alexbezrukov777@ukr.net>
2018-11-27 02:32:58 +0200

    modal window

--- js/script.js
+++ js/script.js
@@ -7,10 +7,10 @@ let chooseBtn = document.getElementById('choose'),
     modal = document.querySelector('.modal'),
     closeBtn = document.querySelector('.close'),
     textArea = document.getElementsByName('message')[0],
-    small = document.querySelector('div h3'),
+    
     overlay = document.querySelector('.overlay')
     thanks = document.querySelector('.thanks'),
-    submit = document.querySelector('button');
+    submit = document.querySelectorAll('button')[1];
     
 
 let username = prompt('Как Ваше имя', "Андрей Иванович"),
@@ -21,21 +21,21 @@ let username = prompt('Как Ваше имя', "Андрей Иванович")
     phoneInput.value = tel,
     mailInput.value = email;
 
-    function hover(){
-        heading.textContent = "Привет"
-    }
-    
-
-    heading.addEventListener("mouseenter", hover);
-    
-    heading.addEventListener("mouseleave",  function (){
-        heading.textContent = "Все включено"
-    });
-
-
     receiveBtn.addEventListener("click", function(){
         modal.style.display = "block"
     });
+
+
+    closeBtn.addEventListener("click", function(){
+        modal.style.display = "none"
+    });
+
+    window.addEventListener("keydown", function(e){
+        if(e.keyCode == 27){
+            modal.style.display = "none"
+        }
+    });
+
 
     submit.addEventListener("click", function(){
         modal.style.display = "none"
@@ -46,12 +46,6 @@ let username = prompt('Как Ваше имя', "Андрей Иванович")
             overlay.style.display = "none"
             thanks.style.display = "none"
         },2000)
-    });
-
-    window.addEventListener("keydown", function(e){
-        if(e.keyCode == 27){
-            modal.style.display = "none"
-        }
     });
 
     nameInput.addEventListener("input", function(){
